d667858299f3b9897c807b534a108b6b4588f367
jimg <jim.grandits@gmail.com>
2019-10-14 18:42:04 -0400

    Add z-index changes

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ function createCardStack() {
     // cards = cardNames;
 
     for (let i = 0; i < cards.length; ++i) {
-        let cardDiv = `<div id='card_${i}' class='card'  draggable='true' droppable='false' ondragstart='drag(event, this)'>${cards[i]}</div>`;
+        let cardDiv = `<div id='card_${i}' class='card'  draggable='true' droppable='false' onclick='cardClick(this)' ondragstart='drag(event, this)'>${cards[i]}</div>`;
         cardStack.innerHTML += cardDiv;
 
-        //document.getElementById(`card_${i}`).style.zIndex = z_hide;
+        document.getElementById(`card_${i}`).style.zIndex = z_hide;
     }
 
-    //onclick='cardClick(this)'
-    //currentCard = document.getElementById('card_0');
-    //currentCard.style.zIndex = z_show;
+    //
+    currentCard = document.getElementById('card_0');
+    currentCard.style.zIndex = z_show;
 
 }
 
